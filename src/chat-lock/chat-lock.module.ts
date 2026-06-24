e42776050b71d6b334f204b75313df4f1449e855
Mykola Ponomarenko <nikolay.pon9912@gmail.com>
2026-06-24 12:48:41 +0300

    - Import missing AppLogger into ChatLockModule

--- src/chat-lock/chat-lock.module.ts
+++ src/chat-lock/chat-lock.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatLockService } from './chat-lock.service';
 import { ChatLockScheduler } from './chat-lock.scheduler';
 import { PrismaService } from '../prisma.service';
+import {AppLogger} from "../logger/app-logger.service";
 
 @Module({
-    providers: [ChatLockService, ChatLockScheduler, PrismaService],
+    providers: [ChatLockService, ChatLockScheduler, PrismaService, AppLogger],
     exports: [ChatLockService],
 })
 export class ChatLockModule {}
